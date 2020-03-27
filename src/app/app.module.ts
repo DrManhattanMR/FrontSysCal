ac67482d62a3e47e1dc0086e843f82c79b4bedac
ManfredDeveloper <developermanfred@gmail.com>
2020-03-26 17:42:07 -0700

    comienzo del administrador del sistema de calificaciones de matuko

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { NewStudentComponent } from './Components/new-student/new-student.component';
+import { NewTeacherComponent } from './Components/new-teacher/new-teacher.component';
+import { GroupComponent } from './Components/group/group.component';
+import { PeriodoComponent } from './Components/periodo/periodo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    NewStudentComponent,
+    NewTeacherComponent,
+    GroupComponent,
+    PeriodoComponent
   ],
   imports: [
     BrowserModule,
